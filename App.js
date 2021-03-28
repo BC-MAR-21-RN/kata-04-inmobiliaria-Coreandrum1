2fb89d43b35d1810ae540e1b629cdf118e1f74b4
Coreandrum1 <47857455+Coreandrum1@users.noreply.github.com>
2021-03-28 11:07:55 -0600

    Added proper data input

--- App.js
+++ App.js
@@ -1,32 +1,48 @@
-import React, { Component } from 'react';
-import {View, SafeAreaView, FlatList, Text, StyleSheet, TextInput, Image, StatusBar} from 'react-native';
-import {Card} from './src/components/card'
+import React, { Component } from "react";
+import {
+  View,
+  SafeAreaView,
+  FlatList,
+  Text,
+  StyleSheet,
+  TextInput,
+  Image,
+  StatusBar,
+} from "react-native";
+import { Card } from "./src/components/card";
 import { DATA } from "./src/data/data";
 // get our fontawesome imports
 
 class App extends Component {
-  render(){
-
-    return(
+  render() {
+    return (
       <SafeAreaView style={styles.container}>
-      <FlatList
-        data={DATA}
-        renderItem={({item})=>(
-          <Card name={item.name} address={item.address} uri={item.uri} bedrooms={item.bedrooms} bathrooms={item.bathrooms} size={item.size} price={item.price}></Card>
+        <FlatList
+          data={DATA}
+          renderItem={({ item }) => (
+            <Card
+              name={item.name}
+              address={item.address}
+              uri={item.uri}
+              bedrooms={item.bedrooms}
+              bathrooms={item.bathrooms}
+              size={item.size}
+              price={item.price}
+              score={item.score}
+            ></Card>
           )}
-        keyExtractor={item => item.id}
-      />
-    </SafeAreaView>
-    )
+          keyExtractor={(item) => item.id}
+        />
+      </SafeAreaView>
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  card: {
+  card: {},
+  container: {
+    flex: 1,
   },
-  container:{
-      flex:1,
-  },
-})
+});
 
 export default App;
--- src/components/card.js
+++ src/components/card.js
@@ -1,96 +1,126 @@
-import React, { Component } from 'react';
-import {View, Text, StyleSheet, TextInput, Image} from 'react-native';
-
-// get our fontawesome imports
-import { Icon } from 'react-native-elements'
+import React, { Component } from "react";
+import { View, Text, StyleSheet, TextInput, Image } from "react-native";
+import { Icon } from "react-native-elements";
 
 export class Card extends Component {
-    constructor(props){
-        super(props)        
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render(){
-        const {name, address, uri, bedrooms, bathrooms, size, price} = this.props
+  render() {
+    const {
+      name,
+      address,
+      uri,
+      bedrooms,
+      bathrooms,
+      size,
+      price,
+      score,
+    } = this.props;
 
-        return(
-        <View style={styles.container}>
-            <View style={styles.leftBox}>
-                <Image source={{uri: `${uri}`}} style={styles.card}></Image>
-                <View style={styles.score}>
-                    <Icon size={18} name='star' color='#eeba00' style={styles.scoreIcon}/>
-                    <Text style={styles.scoreText}>4.7</Text>
-                </View>
-            </View>
-            <View style={styles.rightBox}>
-                <Text style={{fontSize: 20}}>{name}</Text>
-                <Text style={styles.address}>{address}</Text>
-                <View style={styles.specs}>
-                    <Icon size={18} name='bed' type='font-awesome' color='#80838f' style={styles.scoreIcon}/>
-                    <Text style={styles.scoreText2}>{bedrooms}</Text>
-                    <Icon size={18} name='shower' type='font-awesome' color='#80838f' style={styles.scoreIcon}/>
-                    <Text style={styles.scoreText2}>{bathrooms}</Text>
-                    <Icon size={18} name='square' type='font-awesome' color='#80838f' style={styles.scoreIcon}/>
-                    <Text style={styles.scoreText2}>{size} ft²</Text>
-                </View>
-                <Text style={{fontSize: 20}}>${price}/m</Text>
-            </View>
+    return (
+      <View style={styles.container}>
+        <View style={styles.leftBox}>
+          <Image source={{ uri: `${uri}` }} style={styles.card}></Image>
+          <View style={styles.score}>
+            <Icon
+              size={18}
+              name="star"
+              color="#eeba00"
+              style={styles.scoreIcon}
+            />
+            <Text style={styles.scoreText}>{score}</Text>
+          </View>
         </View>
-        )
-    }
+        <View style={styles.rightBox}>
+          <Text style={{ fontSize: 20 }}>{name}</Text>
+          <Text style={styles.address}>{address}</Text>
+          <View style={styles.specs}>
+            <Icon
+              size={18}
+              name="bed"
+              type="font-awesome"
+              color="#80838f"
+              style={styles.scoreIcon}
+            />
+            <Text style={styles.scoreText2}>{bedrooms}</Text>
+            <Icon
+              size={18}
+              name="shower"
+              type="font-awesome"
+              color="#80838f"
+              style={styles.scoreIcon}
+            />
+            <Text style={styles.scoreText2}>{bathrooms}</Text>
+            <Icon
+              size={18}
+              name="square"
+              type="font-awesome"
+              color="#80838f"
+              style={styles.scoreIcon}
+            />
+            <Text style={styles.scoreText2}>{size} ft²</Text>
+          </View>
+          <Text style={{ fontSize: 20 }}>${price}/m</Text>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    card: {
-        height: 110,
-        width: 110,
-        borderRadius: 10
-    },
-    container:{
-        height:150,
-        flexDirection: 'row',
-        backgroundColor: '#E9f6f9',
-        margin: 10,
-        borderRadius: 10,
-        paddingTop: 15,
-        paddingBottom: 15,
-    },
-    leftBox:{
-        flex: 3,
-        justifyContent: 'center',
-        alignItems: 'center',     
-    },
-    rightBox:{
-        flex: 4,
-        justifyContent: 'space-evenly'
-    },
-    address:{
-        color: '#737373'
-    },
-    specs:{
-        flexDirection: 'row',
-    },
-    score:{
-        flex: 1,
-        flexDirection: 'row',
-        position: 'absolute',
-        backgroundColor: '#fbedb7',
-        padding: 7,
-        borderRadius: 10,
-        top: 75
-    },
-    scoreText: {
-        fontWeight: '700',
-        color: '#826b32',
-
-    },
-    scoreText2: {
-        paddingRight: 20
-    },
-    scoreItem:{
-        height: 3,
-        width: 3,
-    },
-    scoreIcon:{
-        paddingRight: 4
-    }
-  })
+  card: {
+    height: 110,
+    width: 110,
+    borderRadius: 10,
+  },
+  container: {
+    height: 150,
+    flexDirection: "row",
+    backgroundColor: "#E9f6f9",
+    margin: 10,
+    borderRadius: 10,
+    paddingTop: 15,
+    paddingBottom: 15,
+  },
+  leftBox: {
+    flex: 3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  rightBox: {
+    flex: 4,
+    justifyContent: "space-evenly",
+  },
+  address: {
+    color: "#737373",
+  },
+  specs: {
+    flexDirection: "row",
+  },
+  score: {
+    flex: 1,
+    flexDirection: "row",
+    position: "absolute",
+    backgroundColor: "#fbedb7",
+    padding: 7,
+    borderRadius: 13,
+    top: 75,
+    paddingRight: 10,
+  },
+  scoreText: {
+    fontWeight: "700",
+    color: "#826b32",
+  },
+  scoreText2: {
+    paddingRight: 20,
+  },
+  scoreItem: {
+    height: 3,
+    width: 3,
+  },
+  scoreIcon: {
+    paddingRight: 4,
+  },
+});
